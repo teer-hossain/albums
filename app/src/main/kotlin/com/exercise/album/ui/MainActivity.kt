781d6fa8a72b368c82f616c25f348eabe8945239
Towsif Hossain <towsif@creamsonservices.com>
2019-06-02 19:20:38 +0530

    Mocking menu added

--- app/src/main/kotlin/com/exercise/album/ui/MainActivity.kt
+++ app/src/main/kotlin/com/exercise/album/ui/MainActivity.kt
@@ -2,9 +2,12 @@ package com.exercise.album.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.exercise.album.R
 import com.exercise.album.ext.addFragment
 import org.jetbrains.anko.find
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setSupportActionBar(find(R.id.toolbar))
         supportActionBar?.setTitle(R.string.app_name)
 
         if (savedInstanceState == null) addFragment(R.id.container, AlbumListFragment.newInstance())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if ((item?.itemId == R.id.action_settings)) {
+            toast("Open settings")
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+
     }
 }
